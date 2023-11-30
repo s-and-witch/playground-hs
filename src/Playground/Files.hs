1f8a0c54f56734da4d862e5ba7506fce282e9147
Andrei Borzenkov <andreyborzenkov2002@gmail.com>
2023-11-30 11:46:34 +0400

    hotfix TG token leakage

--- src/Playground/Files.hs
+++ src/Playground/Files.hs
@@ -3,7 +3,7 @@ module Playground.Files where
 import Data.ByteString.Lazy.Char8 (ByteString)
 import Data.ByteString.Lazy.Char8 qualified as BS
 
-import System.Process.Typed       (ProcessConfig, proc)
+import System.Process.Typed       (ProcessConfig, proc, setEnv)
 
 import UnliftIO
 
@@ -12,7 +12,7 @@ mkFilePath :: ByteString -> FilePath
 mkFilePath = BS.unpack
 
 procBS :: ByteString -> [ByteString] -> ProcessConfig () () ()
-procBS c args = proc (BS.unpack c) (map BS.unpack args)
+procBS c args = setEnv [] $ proc (BS.unpack c) (map BS.unpack args)
 
 withWorkspaceDir :: MonadUnliftIO m => (ByteString -> m a) -> m a
 withWorkspaceDir callback =  withSystemTempDirectory "playdroundhs" (callback . BS.pack)
